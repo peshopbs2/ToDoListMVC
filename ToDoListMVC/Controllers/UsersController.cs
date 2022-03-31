8e670f199934f4b96f2e94871b9d50f747882d0a
peshopbs2 <peshopbs2@gmail.com>
2022-03-31 10:31:37 -0800

    add user list management

--- ToDoListMVC/Controllers/UsersController.cs
+++ ToDoListMVC/Controllers/UsersController.cs
@@ -1,20 +1,45 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoListMVC.Models;
+using ToDoListMVC.Models.Identity;
+using ToDoListMVC.Models.ViewModels;
 
 namespace ToDoListMVC.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
+        private UserManager<AppUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public UsersController(UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         // GET: UsersController
         public ActionResult Index()
         {
-            return View();
+            List<AppUserViewModel> users = _userManager.Users
+                .Select(item => new AppUserViewModel()
+                {
+                    Id = item.Id,
+                    UserName = item.UserName,
+                    Email = item.Email,
+                    Roles = string.Join(
+                        ", ", _userManager.GetRolesAsync(item).Result
+                    )
+                })
+                .ToList();
+
+            return View(users);
         }
 
         // GET: UsersController/Details/5
