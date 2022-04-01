7ca8bab346aff4615051f48d4930f2c09316b617
peshopbs2 <peshopbs2@gmail.com>
2022-04-01 08:13:37 -0800

    add repository template class

---
+++ ToDoListMVC.DAL/Abstractions/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListMVC.DAL.Abstractions
+{
+    public interface IRepository<T>
+    {
+        T Get(Func<T, bool> predicate);
+        T GetById(int id);
+        List<T> GetAll();
+        List<T> Find(Func<T, bool> predicate);
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Remove(T entity);
+        bool RemoveById(int id);
+    }
+}
---
+++ ToDoListMVC.DAL/Repositories/Repository.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListMVC.DAL.Abstractions;
+
+namespace ToDoListMVC.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly DbContext _context;
+        public Repository(DbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public bool Create(T entity)
+        {
+            entity.CreatedAt = DateTime.Now;
+            _context.Set<T>().Add(entity);
+            return _context.SaveChanges() != 0;
+        }
+
+        public List<T> Find(Func<T, bool> predicate)
+        {
+            //.Set<T> tells us which DbSet<T> we use for the given T entity
+            //predicate = lambda expression, ex.: item => item.Value > 0
+            return _context
+                .Set<T>()
+                .Where(predicate)
+                .ToList();
+        }
+
+        public T Get(Func<T, bool> predicate)
+        {
+            return _context.
+                Set<T>()
+                .FirstOrDefault(predicate);
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public bool Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return _context.SaveChanges() != 0;
+        }
+
+        public bool RemoveById(int id)
+        {
+            var item = _context.Set<T>().FirstOrDefault(item => item.Id == id);
+            if (item != null)
+            {
+                _context.Set<T>()
+                    .Remove(item);
+                return _context.SaveChanges() != 0;
+
+            }
+            else
+            {
+                //I'm deleting non-existent ID
+                return false;
+            }
+        }
+
+        public bool Update(T entity)
+        {
+            if (entity.Id != 0)
+            {
+                entity.ModifiedAt = DateTime.Now;
+                _context.Update(entity);
+                return _context.SaveChanges() != 0;
+            }
+            else
+            {
+                //Idk what to change because no ID
+                return false;
+            }
+        }
+    }
+}
