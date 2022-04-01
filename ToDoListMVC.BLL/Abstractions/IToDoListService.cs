79d9d26c7ffbe882e53d9e53390014c3d23d41a6
peshopbs2 <peshopbs2@gmail.com>
2022-04-01 08:54:03 -0800

    add to do list business logic

---
+++ ToDoListMVC.BLL/Abstractions/IToDoListService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListMVC.DAL.Entities;
+
+namespace ToDoListMVC.BLL.Abstractions
+{
+    public interface IToDoListService
+    {
+        bool Create(string title);
+        bool Update(int toDoListId, string title);
+        List<ToDoList> GetAll();
+        ToDoList GetToDoListById(int toDoListId);
+        bool Remove(int toDoListId);
+        List<ToDoList> GetToDoListsByUser(string userId);
+    }
+}
---
+++ ToDoListMVC.BLL/Services/ToDoListService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListMVC.BLL.Abstractions;
+using ToDoListMVC.DAL.Abstractions;
+using ToDoListMVC.DAL.Entities;
+
+namespace ToDoListMVC.BLL.Services
+{
+    public class ToDoListService : IToDoListService
+    {
+        private readonly IRepository<ToDoList> _toDoListRepository;
+        public ToDoListService(IRepository<ToDoList> toDoListRepostiory)
+        {
+            _toDoListRepository = toDoListRepostiory;
+        }
+
+        public bool Create(string title)
+        {
+            return _toDoListRepository.Create(new ToDoList()
+            {
+                Title = title
+            });
+        }
+
+        public List<ToDoList> GetAll()
+        {
+            return _toDoListRepository.GetAll();
+        }
+
+        public ToDoList GetToDoListById(int toDoListId)
+        {
+            return _toDoListRepository.GetById(toDoListId);
+        }
+
+        public List<ToDoList> GetToDoListsByUser(string userId)
+        {
+            //TODO: change the predicate so we see shared and not only created by us lists
+            return _toDoListRepository
+                .Find(item => item.CreatedBy == userId);
+        }
+
+        public bool Remove(int toDoListId)
+        {
+            return _toDoListRepository.Remove(
+                _toDoListRepository.GetById(toDoListId)
+            );
+        }
+
+        public bool Update(int toDoListId, string title)
+        {
+            var toDoList = _toDoListRepository.GetById(toDoListId);
+            toDoList.Title = title;
+            return _toDoListRepository.Update(toDoList);
+        }
+    }
+}
