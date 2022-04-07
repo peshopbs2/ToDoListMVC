97b0a2e084a1f69ec3b36641cf028f443cf2174f
peshopbs2 <peshopbs2@gmail.com>
2022-04-07 09:47:48 -0800

    add todo item assign entity

--- ToDoListMVC.DAL/Entities/ToDoItem.cs
+++ ToDoListMVC.DAL/Entities/ToDoItem.cs
@@ -8,10 +8,15 @@ namespace ToDoListMVC.DAL.Entities
 {
     public class ToDoItem : BaseEntity
     {
+        public ToDoItem()
+        {
+            Assigns = new List<ToDoItemAssign>();
+        }
         public int ToDoListId { get; set; }
         public virtual ToDoList ToDoList { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public bool IsComplete { get; set; }
+        public virtual List<ToDoItemAssign> Assigns { get; set; }
     }
 }
---
+++ ToDoListMVC.DAL/Entities/ToDoItemAssign.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListMVC.DAL.Entities
+{
+    public class ToDoItemAssign : BaseEntity
+    {
+        public int ToDoItemId{ get; set; }
+        public virtual ToDoItem ToDoItem { get; set; }
+        public string UserId { get; set; }
+    }
+}
